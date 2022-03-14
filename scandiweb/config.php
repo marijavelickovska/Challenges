0e4b7507ab44961b91aeb68540713850bd22f0b8
marijavelickovska <92168762+marijavelickovska@users.noreply.github.com>
2022-03-14 20:55:19 +0100

    Delete scandiweb directory

--- scandiweb/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("DBNAME", "scandiweb-demo");
-define("DBUSER", "root");
-define("DBPASS", "");
-define("URL", "http://http://localhost/scandiweb/"); // Change this to match your root
-
